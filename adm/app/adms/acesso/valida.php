ab78e5557780bfe45fe160ad2b5ae86f651bdaf5
pedroBaptistaDosSantos <pedro.santos415@etec.sp.gov.br>
2020-11-04 23:33:46 -0300

    Permitir acesso a página ao nível de acesso

--- adm/app/adms/acesso/valida.php
+++ adm/app/adms/acesso/valida.php
@@ -15,13 +15,17 @@ if ($sendLogin) {
             $row_user = mysqli_fetch_assoc($resultado_user);
 
             if (password_verify($senha, $row_user['senha'])) {
+                $result_niv_ac = "SELECT ordem FROM adms_niveis_acessos WHERE id = " . $row_user['adms_niveis_acesso_id'] . " LIMIT 1";
+                $resultado_niv_ac = mysqli_query($conn, $result_niv_ac);
+                $row_niv_ac =  mysqli_fetch_assoc($resultado_niv_ac);
+                
                 $_SESSION['id'] = $row_user['id'];
                 $_SESSION['nome'] = $row_user['nome'];
                 $_SESSION['email'] = $row_user['email'];
                 $_SESSION['adms_niveis_acesso_id'] = $row_user['adms_niveis_acesso_id'];
+                $_SESSION['ordem'] = $row_niv_ac['ordem'];
                 $url_destino = pg . '/visualizar/home';
                 header("Location: " . $url_destino);
-                
             } else {
                 $_SESSION['msg'] = "<div class=\"alert alert-danger\" role=\"alert\"> Login ou senha inválidos </div>";
                 $url_destino = pg . '/acesso/login';
--- adm/index.php
+++ adm/index.php
@@ -7,11 +7,21 @@ include_once './config/conexao.php';
 include_once './lib/lib_valida.php';
 $url = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_STRING);
 $url_limpa = limparUrl($url);
-$result_pg = "SELECT * FROM adms_paginas"
-        . ""
-        . " WHERE endereco='" . $url_limpa . "' "
-        . "AND adms_sits_pg_id  =1 "
-        . "LIMIT 1";
+
+if (isset($_SESSION['adms_niveis_acesso_id'])) {
+    $adms_niveis_acesso_id = $_SESSION['adms_niveis_acesso_id'];
+} else {
+    $adms_niveis_acesso_id = 0;
+}
+$result_pg = "SELECT pg.tp_pagina, pg.endereco "
+        . " FROM adms_paginas pg"
+        . " LEFT JOIN adms_nivac_pgs nivpg "
+        . " ON nivpg.adms_pagina_id = pg.id "
+        . " WHERE pg.endereco='" . $url_limpa . "' "
+        . " AND (pg.adms_sits_pg_id = 1"
+        . " AND (adms_niveis_acessos_id = '" . $adms_niveis_acesso_id . "'  "
+        . " AND nivpg.permissao = 1) OR (pg.lib_pub = 1))"
+        . " LIMIT 1";
 $resultado_pg = mysqli_query($conn, $result_pg);
 ?>
 <!DOCTYPE html5>
@@ -23,10 +33,13 @@ $resultado_pg = mysqli_query($conn, $result_pg);
         if (file_exists($file)) {
             include $file;
         } else {
-            include 'app/adms/visualizar/home.php';
+            //include 'app/adms/visualizar/home.php';
+            $url_destino = pg . '/acesso/login';
+            header("Location: " . $url_destino);
         }
     } else {
-        include 'app/adms/visualizar/home.php';
+        $url_destino = pg . '/acesso/login';
+        header("Location: " . $url_destino);
     }
     ?>
 </html>
