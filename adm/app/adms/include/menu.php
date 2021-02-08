6dafc8d5008340fbc91017a21ef8f8de98d0a24e
pedroBaptistaDosSantos <pedro.santos415@etec.sp.gov.br>
2021-02-08 17:21:19 -0300

    Inserindo dropdown no menu vertical e tela listar usuários

--- adm/app/adms/include/menu.php
+++ adm/app/adms/include/menu.php
@@ -1,23 +1,43 @@
 <nav class="sidebar bg-dark">
     <ul class="list-unstyled">
         <?php
-         $result_menu = "SELECT nivpg.*, 
-                men.nome, men.icone, 
-                pg.nome_pagina, pg.endereco, pg.icone
+        $result_menu = "SELECT nivpg.*, 
+                men.nome, men.icone iconmen, 
+                pg.nome_pagina, pg.endereco, pg.icone iconpg
                 FROM adms_nivac_pgs nivpg
                 INNER JOIN adms_menus men ON men.id=nivpg.adms_menu_id
                 INNER JOIN adms_paginas pg ON pg.id=nivpg.adms_pagina_id
             WHERE nivpg.adms_niveis_acessos_id='" . $_SESSION['adms_niveis_acesso_id'] . "' 
                 AND nivpg.permissao=1 
                 AND nivpg.lib_menu=1";
-            $resultado_menu = mysqli_query($conn, $result_menu);
+        $resultado_menu = mysqli_query($conn, $result_menu);
 
-            while ($row_menu = mysqli_fetch_assoc($resultado_menu)) {
-                //echo 'ID: ' . $row_menu['id'] . "<br>";
-                //echo "<i class='".$row_menu['icone']."'></i>".$row_menu['nome']."<br>";
-                echo "<li><a href='".pg."/".$row_menu['endereco']."'><i class='".$row_menu['icone']."'></i> ". $row_menu['nome'] ."</a></li>";
+        while ($row_menu = mysqli_fetch_assoc($resultado_menu)) {
+            if ($row_menu['dropdown'] == 1) {
+                ?>
+                <li>
+                    <a href="#submenu1" data-toggle="collapse"> 
+                        <i class="fas fa-user"></i>Usuarios
+                    </a>
+                    <ul class="list-unstyled collapse" id="submenu1">
+                        <li>
+                            <a href="listar.html"><i class="fas fa-chevron-right"></i>
+                                <i class="fas fa-users"></i>
+                                Usuários</a>
+                        </li>
+                        <li><a href="#">
+                                <i class="fas fa-chevron-right"></i>
+                                <i class="fas fa-key"></i>
+                                Nivel de acesso</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <?php
+            } else {
+                echo "<li><a href='" . pg . "/" . $row_menu['endereco'] . "'><i class='" . $row_menu['iconmen'] . "'></i> " . $row_menu['nome'] . "</a></li>";
             }
-        
+        }
         ?>
         <li><a href="#"><i class="fas fa-tachometer-alt "></i> Dashboard</a></li>
         <li>
--- adm/app/adms/visualizar/home.php
+++ adm/app/adms/visualizar/home.php
@@ -16,25 +16,29 @@ include_once 'app/adms/include/head.php';
         ?>    
         </<div>
             <?php
-            echo "<br>Bem vindo a home";
-            echo "<br> <a href='" . pg . "/acesso/sair'> Sair </a>";
+            echo "Bem vindo a home <br>";
+            echo " <a href='" . pg . "/acesso/sair'> Sair </a> <br>";
 
             $result_menu = "SELECT nivpg.*, 
-                men.nome, men.icone
+                men.nome, men.icone iconmen, 
+                pg.nome_pagina, pg.endereco, pg.icone iconpg
                 FROM adms_nivac_pgs nivpg
                 INNER JOIN adms_menus men ON men.id=nivpg.adms_menu_id
+                INNER JOIN adms_paginas pg ON pg.id=nivpg.adms_pagina_id
             WHERE nivpg.adms_niveis_acessos_id='" . $_SESSION['adms_niveis_acesso_id'] . "' 
                 AND nivpg.permissao=1 
                 AND nivpg.lib_menu=1";
+
+
             $resultado_menu = mysqli_query($conn, $result_menu);
 
-            while ($row_menu = mysqli_fetch_assoc($resultado_menu)) {
+            while($row_menu = mysqli_fetch_assoc($resultado_menu)) {
                 //echo 'ID: ' . $row_menu['id'] . "<br>";
-                echo "<i class='".$row_menu['icone']."'></i>".$row_menu['nome']."<br>";
-                
+                echo "Nome icone: " . $row_menu['iconmen'] . "    -   " . $row_menu['nome'] . "    -   " . $row_menu['endereco'] . "<br>";
             }
-            
+
             include_once 'app/adms/include/rodape_lib.php';
             ?>
+
             </body>
 
