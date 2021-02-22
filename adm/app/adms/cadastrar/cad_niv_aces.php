f9785919a1f1838d2ddf92fa8318bdc5e7e9a35c
pedroBaptistaDosSantos <pedro.santos415@etec.sp.gov.br>
2021-02-22 19:05:58 -0300

    Criação do formulario de cadastro de nivel de acesso e criação da pagina responsavel pelo processo

--- adm/app/adms/cadastrar/cad_niv_aces.php
+++ adm/app/adms/cadastrar/cad_niv_aces.php
@@ -1,8 +1,51 @@
 <?php
-if (!isset($seg)){
+if (!isset($seg)) {
     exit;
 }
+include_once 'app/adms/include/head.php';
+?>
+<body>
 
-var_dump(limparUrl($url));
-echo 'Cadastrar Nivel de Acesso<br>';
-var_dump($url);
+
+    <?php
+    include_once 'app/adms/include/header.php';
+    ?>
+    <div class="d-flex">
+
+        <?php
+        include_once 'app/adms/include/menu.php';
+        ?>
+        <div class="content p-1">
+            <div class="list-group-item">
+                <div class=" d-flex">
+                    <div class="mr-auto p2">
+                        <h2 class="display-4 titulo">Cadastrar Nível de Acesso</h2>
+                    </div>    
+
+                    <div class="p-2">
+                        <?php
+                        $btn_list = carregar_btn('visualizar/vis_niv_aces', $conn);
+                        if ($btn_list) {
+                            echo "<a href='" . pg . "/listar/list_niv_aces' class='btn btn-outline-info btn-sm'>Listar </a>   ";
+                        }
+                        ?>                     
+                    </div>
+                </div> <hr>
+                <form class="text-left" method="POST" action="<?php echo pg;?>/processa/proc_cad_niv_aces">
+                    <div class="form-group">
+                        <label> <span class="text-danger">*</span> Nome</label>
+                        <input name="nome" type="text" class="form-control" placeholder=" do nível de acesso">
+                    </div>
+                    <p>
+                        <span class="text-danger">*</span> Campo Obrigatorio
+                    </p>
+                    <input name="SendCadNivAc" type="submit" class="btn btn-sucess" value="Cadastrar">
+                </form>    
+            </div>    
+        </div>
+        <?php
+        include_once 'app/adms/include/rodape_lib.php';
+        ?>
+
+    </div>
+</body>
---
+++ adm/app/adms/processa/proc_cad_niv_aces.php
@@ -0,0 +1,8 @@
+<?php
+if (!isset($seg)){
+    exit;
+}
+
+var_dump(limparUrl($url));
+echo 'processa Cadastro de Nivel de Acesso<br>';
+var_dump($url);
